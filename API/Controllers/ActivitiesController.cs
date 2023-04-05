ca30b9f5262d6c7ee71b8d41483f9bcb5702b964
aliinorouzian <aliinorouzian@gmail.com>
2023-04-05 11:49:29 +0330

    Make controller thinner.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,17 +8,10 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet] // api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // api/activities/sfdsdfsd
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,6 +7,11 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public abstract class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
 
+        // if _mediator is null return right !
+        // protected here is for drived class can access it.
+        protected IMediator Mediator => _mediator ??=
+         HttpContext.RequestServices.GetService<IMediator>();
     }
 }
