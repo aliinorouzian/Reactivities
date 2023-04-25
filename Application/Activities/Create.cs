bcf494e1a755a810a6f0234c95d945e2d0f1854c
aliinorouzian <aliinorouzian@gmail.com>
2023-04-25 19:39:27 +0330

    Add attendees to activity table. (many to many)

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -30,15 +32,30 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             // return Unit is like void for stand return nothing.
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                // accessor is like User.identity.name in here
+                var user = await _context.Users.FirstOrDefaultAsync(u =>
+                    u.UserName == _userAccessor.GetUsername());
+
+                var attendee = new ActivityAttendee()
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true,
+                };
+
+                request.Activity.Attendees.Add(attendee);
+
                 await _context.Activities.AddAsync(request.Activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
