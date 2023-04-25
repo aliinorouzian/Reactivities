f21888d0ece3443195dae97e7a4da5320905b06a
aliinorouzian <aliinorouzian@gmail.com>
2023-04-25 19:31:59 +0330

    Create a http accessor for get username more easily.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Activities;
 using Application.Core;
+using Application.Interfaces;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -43,6 +45,10 @@ namespace API.Extensions
             // FluentValidator
             services.AddFluentValidationAutoValidation();
             services.AddValidatorsFromAssemblyContaining<Create>();
+
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<IUserAccessor, UserAccessor>();
 
             return services;
         }
---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUsername()
+        {
+            return _httpContextAccessor.HttpContext.User
+                .FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
