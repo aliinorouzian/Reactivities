0a13cf19ea9845a810e8c31933a48f12b4ecb915
aliinorouzian <aliinorouzian@gmail.com>
2023-04-25 18:08:22 +0330

    Add register endpoint.

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -16,6 +16,7 @@ namespace API.Extensions
             services.AddIdentityCore<AppUser>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
+                opt.User.RequireUniqueEmail = true;
             })
                 .AddEntityFrameworkStores<DataContext>();
 
